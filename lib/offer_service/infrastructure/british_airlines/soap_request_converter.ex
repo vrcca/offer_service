8bfae19a76a0a2a86c49c19b9010af04182c7d34
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-07-06 16:55:33 -0300

    Moves eex template to same directory of converter

--- lib/offer_service/infrastructure/british_airlines/soap_request_converter.ex
+++ lib/offer_service/infrastructure/british_airlines/soap_request_converter.ex
@@ -2,7 +2,7 @@ defmodule OfferService.Infrastructure.BritishAirlines.SoapRequestConverter do
   require EEx
   alias OfferService.Domain.FlightPreferences
 
-  @template_file "./priv/airlines/british_airlines/request_template.eex"
+  @template_file __DIR__ <> "/request_template.eex"
 
   EEx.function_from_file(
     :def,
