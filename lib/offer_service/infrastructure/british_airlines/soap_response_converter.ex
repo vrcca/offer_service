831506b46802f2a6a6c54371d280c7704a24517a
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-07-06 20:04:47 -0300

    Response converter now returns a stream

--- lib/offer_service/infrastructure/british_airlines/soap_response_converter.ex
+++ lib/offer_service/infrastructure/british_airlines/soap_response_converter.ex
@@ -3,11 +3,10 @@ defmodule OfferService.Infrastructure.BritishAirlines.SoapResponseConverter do
 
   alias OfferService.Domain.Offer
 
-  def convert(payload) do
+  def convert_to_domain_stream(payload) do
     payload
     |> stream_tags([:AirlineOffer], discard: [:AirlineOffer])
     |> to_domain_stream()
-    |> get_first()
   end
 
   defp convert_price_to_cents(price) do
@@ -28,15 +27,5 @@ defmodule OfferService.Infrastructure.BritishAirlines.SoapResponseConverter do
       airline = offer |> xpath(~x"./OfferID/@Owner"s)
       Offer.new(price: price, airline: airline)
     end)
-  end
-
-  defp get_first(offers) do
-    offers
-    |> Stream.take(1)
-    |> Enum.to_list()
-    |> case do
-      [] -> nil
-      [offer] -> offer
-    end
   end
 end
--- test/offer_service/infrastructure/british_airlines/soap_response_converter_test.exs
+++ test/offer_service/infrastructure/british_airlines/soap_response_converter_test.exs
@@ -7,11 +7,11 @@ defmodule OfferService.Infrastructure.BritishAirlines.SoapResponseConverterTest 
 
   test "correctly retrieves the first price from payload" do
     payload = File.read!("test/samples/british_airlines_airshopping_sample.xml")
-    response = @response_converter.convert(payload)
-    assert a_offer(price: 9414, airline: "BA") == response
+    response = @response_converter.convert_to_domain_stream(payload) |> Enum.take(1)
+    assert [a_offer(price: 9414, airline: "BA")] == response
   end
 
   test "empty payload should return empty responses" do
-    assert nil == @response_converter.convert("<Empty></Empty>")
+    assert [] == @response_converter.convert_to_domain_stream("<Empty></Empty>") |> Enum.take(1)
   end
 end
