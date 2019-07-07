61654cf2236512a91852ab2682979e9134155e07
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-07-07 16:47:43 -0300

    Removes duplicated configuration from release

--- config/releases.exs
+++ config/releases.exs
@@ -1,12 +1,5 @@
 import Config
 
-alias OfferService.Infrastructure.{AirFrance, BritishAirlines}
-
-config :offer_service, :airline_repositories, [
-  AirFrance.AirlineSoapRepository,
-  BritishAirlines.AirlineSoapRepository
-]
-
 config :offer_service, british_airlines_api_key: System.fetch_env!("BA_API_KEY")
 config :offer_service, british_airlines_api_url: System.fetch_env!("BA_API_URL")
 
